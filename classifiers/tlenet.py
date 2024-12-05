7830095d803239cdb3ad69976c82600485096d7d
m&m <38250127+mohsafer@users.noreply.github.com>
2024-12-05 11:31:03 +0330

    d

--- classifiers/tlenet.py
+++ classifiers/tlenet.py
@@ -71,8 +71,7 @@ class Classifier_TLENET:
         
         model = keras.models.Model(inputs=input_layer,outputs=output_layer)
         
-        model.compile(optimizer=keras.optimizers.Adam(lr=0.01,decay=0.005),
-                      loss='categorical_crossentropy', metrics=['accuracy'])
+        model.compile(optimizer=keras.optimizers.Adam,loss='categorical_crossentropy', metrics=['accuracy'])
         
         file_path = self.output_directory+'best_model.hdf5'
 
